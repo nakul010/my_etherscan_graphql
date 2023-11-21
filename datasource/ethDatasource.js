028fb608c12845531bfc882658449794a8cc7b94
Nakul Gupta <nakul101gupta@gmail.com>
2023-11-21 11:05:34 +0530

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,51 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+// Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+// Etherscan Data Source Class 
+class EtherDataSource extends RESTDataSource {
+
+  constructor() {
+    super();
+
+    // Base URL for Etherscan API 
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    
+    // Get account balance by address from Etherscan API
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async totalSupplyOfEther() {
+    
+    // Get total ether supply from Etherscan API
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  // Get latest Ethereum price
+  async getLatestEthereumPrice() {
+    
+    // Get latest ether price from Etherscan API
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  // Get block confirmation time
+  async getBlockConfirmationTime() {
+    
+    // Get block confirmation time estimate from Etherscan API 
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+}
+
+module.exports = EtherDataSource;
